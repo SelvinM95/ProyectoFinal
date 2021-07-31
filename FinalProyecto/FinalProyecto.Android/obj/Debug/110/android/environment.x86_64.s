d27fbf32f5a5929f239aed60c2b279d8735c2130
SelvinM95 <83553307+SelvinM95@users.noreply.github.com>
2021-07-30 19:17:52 -0600

    ActualizadoFoto

--- FinalProyecto/FinalProyecto.Android/obj/Debug/110/android/environment.x86_64.s
+++ FinalProyecto/FinalProyecto.Android/obj/Debug/110/android/environment.x86_64.s
@@ -72,7 +72,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"1ea1493e-53ee-4264-9bd7-f2d5dcded9c3"
+	.asciz	"52d7de1a-3055-4147-b0c7-426f0f84a3b6"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
--- FinalProyecto/FinalProyecto/RegistroLogin.xaml.cs
+++ FinalProyecto/FinalProyecto/RegistroLogin.xaml.cs
@@ -80,7 +80,7 @@ namespace FinalProyecto
         public static string GeneratePassword(int longitud)
         {
             string contraseña = string.Empty;
-            string[] letras = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+            string[] letras = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
                                 "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
             Random EleccionAleatoria = new Random();
 
