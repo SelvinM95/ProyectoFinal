c9a075bfa799e682fc8120ba7d24e00559f824b2
SelvinM95 <83553307+SelvinM95@users.noreply.github.com>
2021-08-03 13:55:45 -0600

    NombreApp Cambiado

--- FinalProyecto/FinalProyecto.Android/MainActivity.cs
+++ FinalProyecto/FinalProyecto.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.Gms.Common;
 
 namespace FinalProyecto.Droid
 {
-    [Activity(Label = "FinalProyecto", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "DockShare", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
