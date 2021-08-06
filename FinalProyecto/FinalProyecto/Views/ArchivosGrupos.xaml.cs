fc42f7dfe305b0615655ae843ca00beecd476138
Selvin M <onan_maldonado@hotmail.com>
2021-08-06 11:45:54 -0600

    archivosgrupos general

--- FinalProyecto/FinalProyecto/Views/ArchivosGrupos.xaml.cs
+++ FinalProyecto/FinalProyecto/Views/ArchivosGrupos.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Acr.UserDialogs;
+using FinalProyecto.Classes;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -13,28 +15,29 @@ namespace FinalProyecto.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ArchivosGrupos : ContentPage
     {
-        public ObservableCollection<Card> ListDetails { get; set; }
+         
         public ArchivosGrupos()
         {
             InitializeComponent();
-            ListDetails = new ObservableCollection<Card>
-            {
-                  new Card{ImgIcon="profileUser.png", Name= "BERET - DIE...160K).mp3", Fecha = "6 abr. 2021" },
-                  new Card{ImgIcon="profileUser.png", Name= "BERET - DIE...160K).mp3", Fecha = "6 abr. 2021" },
-                  new Card{ImgIcon="profileUser.png", Name= "BERET - DIE...160K).mp3", Fecha = "6 abr. 2021" },
-                  new Card{ImgIcon="profileUser.png", Name= "BERET - DIE...160K).mp3", Fecha = "6 abr. 2021" },
-                  new Card{ImgIcon="profileUser.png", Name= "BERET - DIE...160K).mp3", Fecha = "6 abr. 2021" }
-            };
-            BindingContext = this;
         }
 
-        public class Card
+        protected async override void OnAppearing()
         {
-            public string ImgIcon { get; set; }
-            public string Name { get; set; }
-            public string Fecha { get; set; }
+            base.OnAppearing();
 
+            UserDialogs.Instance.ShowLoading("Cargando");
+
+            string url = string.Format("http://3.15.208.156/WSXamarin/files/getgroupfiles/{0}", App.Current.Properties["idGroup"]);
+            ConsultManager manager = new ConsultManager();
+            var res = await manager.getFile(url);
+
+            if (res != null)
+            {
+                ListStudent.ItemsSource = res;
+            }
+            UserDialogs.Instance.HideLoading();
         }
+
 
         private async void AudioButton_Clicked(object sender, EventArgs e)
         {
@@ -76,12 +79,12 @@ namespace FinalProyecto.Views
             await Navigation.PushAsync(new GrupoVideoList());
         }
 
-        private async void todoArchivoButton_Clicked(object sender, EventArgs e)
+        private  void todoArchivoButton_Clicked(object sender, EventArgs e)
         {
           
         }
 
-        private async void imagetodoArchivo_Tapped(object sender, EventArgs e)
+        private  void imagetodoArchivo_Tapped(object sender, EventArgs e)
         {
             
         }
